556c0fa9f5f72ddf0461b5385c658f807f4badbf
Michael Salama <michael.fawzy91@yahoo.com>
2018-01-22 18:54:39 +0200

    Rubbish script added

--- Assets/Scripts/Rubbish.cs
+++ Assets/Scripts/Rubbish.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rubbish : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+<<<<<<< HEAD
+            GameManager gameManager =GameObject.FindObjectOfType<GameManager>();
+            gameManager.Rubbishcount++;
+=======
            // Debug.Log("da5al el zebala");
             Destroy(this.gameObject);
 
+>>>>>>> 6cca3f9295d9f4c205c56b239605761b7be3bbeb
 
+            //Debug.Log("da5al el zebala");
+            Destroy(this.gameObject);
         }
     }
    
+
+
 }
