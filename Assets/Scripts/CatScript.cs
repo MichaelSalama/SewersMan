70dda1cd2855b375d46fa45ca70e804bdaf7b963
N man <nader.ma@live.com>
2018-01-22 14:05:06 +0200

    Cat Addition

---
+++ Assets/Scripts/CatScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour {
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            anim.SetTrigger("scared");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            anim.SetTrigger("scared");
+        }
+    }
+
+}
