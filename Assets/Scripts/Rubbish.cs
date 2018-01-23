d1ee8568ee85fe8074b2aa659bcc40b255aa0c0c
N man <nader.ma@live.com>
2018-01-23 09:16:45 +0200

    added spries for balacon in Sprites folder, fixed Rubbish.cs

--- Assets/Scripts/Rubbish.cs
+++ Assets/Scripts/Rubbish.cs
@@ -9,14 +9,10 @@ public class Rubbish : MonoBehaviour {
     {
         if (collision.transform.tag == "Player")
         {
-<<<<<<< HEAD
             GameManager gameManager =GameObject.FindObjectOfType<GameManager>();
-            gameManager.Rubbishcount++;
-=======
+            //gameManager.Rubbishcount++;
            // Debug.Log("da5al el zebala");
             Destroy(this.gameObject);
-
->>>>>>> 6cca3f9295d9f4c205c56b239605761b7be3bbeb
 
             //Debug.Log("da5al el zebala");
             Destroy(this.gameObject);
