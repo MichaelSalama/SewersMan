f03624a0792e7e71393b993ad5d45d79d0708bf2
N man <nader.ma@live.com>
2018-01-24 10:17:48 +0200

    fixed UI

---
+++ Assets/Scripts/PlayBtn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayBtn : MonoBehaviour
+{
+    
+    //private void Start()
+    //{
+    //    DisEnable();
+    //}
+    //public void DisEnable()
+    //{
+    //    GameObject.Find("GameManager").GetComponent<GameManager>().enabled = false;
+    //    GameObject.FindWithTag("Player").GetComponent<PlayerControl>().enabled = false;
+    //    GameObject.FindWithTag("elset").GetComponent<ElWelia>().enabled = false;
+    //}
+
+    public void Enable()
+    {
+        //Debug.Log("Hiiiii");
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().enabled = true;
+        Debug.Log("game manager");
+        GameObject.FindWithTag("Player").GetComponent<PlayerControl>().enabled = true;
+        GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>().enabled = true;
+        GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().gravityScale = 1;
+        Debug.Log("player");
+
+        GameObject.FindWithTag("elset").GetComponent<ElWelia>().enabled = true;
+        Debug.Log("woman");
+        GameObject.FindWithTag("UI").GetComponent<Canvas>().enabled = false; 
+        Debug.Log("UI");
+    }
+
+
+
+}
---
+++ Assets/Scripts/QuitBtn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitBtn : MonoBehaviour {
+    public void BtnQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+	
+}
